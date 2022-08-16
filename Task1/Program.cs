b07080e4bb1055ece8b248ea6f80359965ac983e
VeraMaminova <maminovavv@yandex.ru>
2022-08-17 00:11:23 +0300

    Add Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+
+// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//[1,0,1,1,0,1,0,0]
+
+int Prompt(string message)
+{
+    System.Console.Write(message);           // Вывести сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат функции
+}
+int ReverseNumber(int numb)
+{
+    int res = 0;
+    while (numb > 0)
+    {
+        int temp = numb % 10;
+        numb = numb / 10;
+        res = res * 10 + temp;
+    };
+    return res;
+}
+bool CheckPalindrom(int value1, int value2)
+{
+
+    if (value1 == value2)
+    {
+        return true;
+    }
+    return false;
+}
+int numb1 = Prompt("Введите число >");
+int numb2 = ReverseNumber(numb1);
+
+bool IsPalindrome = CheckPalindrom(numb1, numb2);
+if (IsPalindrome)
+{
+    System.Console.WriteLine($"Число {numb1} -  палиндром");
+}
+else
+{
+    System.Console.WriteLine($"Число {numb1} - не палиндром");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);           // Вывести сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);       // Преобразует строку в целое число
+    return result;                           // Возвращает результат функции
+}
+
+const int X = 0;
+const int Y = 1;
+const int Z = 2;
+
+int[] InputCoords(string message)
+{
+    int[] NumCoord = new int[3];
+    for (int i = 0; i < NumCoord.Length; i++)
+    {
+        System.Console.WriteLine($"{message} {i}- я ");
+        string value = Console.ReadLine();
+        NumCoord[i] = int.Parse(value);
+    }
+    return NumCoord;
+}
+
+double Power2FromPoint(double C1, double C2)
+{
+    double distanceCoords = C1 - C2;
+    return distanceCoords * distanceCoords;
+}
+
+
+double LineLong(int[] point1, int[] point2)
+{
+    double longLine = Math.Sqrt(Power2FromPoint(point1[X], point2[X]) + Power2FromPoint(point1[Y], point2[Y]) + Power2FromPoint(point1[Z], point2[Z]));
+    return longLine;
+}
+int [] point1 = InputCoords("Координаты первой точки");
+int [] point2 = InputCoords("Координаты второй точки");
+System.Console.Write($"Расстояние между точками {LineLong(point1, point2)}");
